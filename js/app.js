f43c8348d1b0b9020ed70d4f3a2c86606bde4eb9
Adam Jimenez <outlandico@gmail.com>
2024-03-13 21:46:53 -0700

    corrected my answer for question 3

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) {
-    let answer = a + b;
-    let words = `The sum of ${a} and ${b} is ${answer}.`;
-    return [answer, words];
+let answer = a + b;
+let words = `The sum of ${a} and ${b} is ${answer}.`;
+return [answer, words];
 }
 
 
@@ -59,12 +59,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-    let product = a * b * c;
-    let words = `The product of ${a}, ${b}, and ${c} is ${product}.`;
-    return [product, words];
+    let sumResult = sum(sum(a, b)[0], c)[0];
+    let productResult = multiply(multiply(a, b)[0], c)[0];
+    let words = `The product of ${a}, ${b}, and ${c} is ${productResult}.`;
+    return [sumResult, productResult, words];
 }
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
